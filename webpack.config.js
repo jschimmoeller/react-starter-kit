64f8f4b800c484176c48d3b88e5a15c3c0e85ab5
jschimmoeller <jschimmoeller@schimmoeller.net>
2016-07-13 20:46:15 -0400

    initial

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports =
+{
+    context: __dirname + "/src",
+    entry: {
+      javascript: "./app.js",
+      html: "./index.html",
+    },
+    output:
+	{
+        path: __dirname+'/dist',
+        filename: "app.js"
+    },
+    module:
+	{
+        loaders:
+		[
+	    	{ test: /(\.jsx$|\.js$)/, loaders: ['react-hot-loader', "babel-loader"], exclude: /node_modules/ },
+        {
+          test: /\.html$/,
+          loader: "file?name=[name].[ext]",
+        },
+	    	{ test: /\.css$/, loader: "style!css", exclude: /node_modules/ },
+ {
+          test: /.*\.(gif|png|jpe?g|svg)$/i,
+          loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+          ]
+        }
+        ]
+    }
+};
